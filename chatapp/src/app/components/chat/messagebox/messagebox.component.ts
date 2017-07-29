558e4c2ede1b4d57ee0dd8899e0971aa37448099
vi-v <vvijayan@purdue.edu>
2017-07-29 22:55:00 +0530

    Update message schema

--- chatapp/src/app/components/chat/messagebox/messagebox.component.ts
+++ chatapp/src/app/components/chat/messagebox/messagebox.component.ts
@@ -14,8 +14,11 @@ export class MessageboxComponent implements OnInit, OnDestroy {
 
   public messages: Message[];
   private connection: any;
+  private prevMessageType: string;
 
   constructor(private _chatService: ChatService) {
+    this.prevMessageType = 'client'
+
     this.messages = [
       {
         message: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
@@ -36,6 +39,12 @@ export class MessageboxComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.connection = this._chatService.getMessages().subscribe((message: Message) => {
+
+      if(this.prevMessageType === 'client') {
+        message.continuous = false
+        this.prevMessageType = 'server'
+      }
+
       this.messages.push(message)
       setTimeout( () => this.scrollToBottom(), 200)
     })
--- chatapp/src/app/interfaces/Message.ts
+++ chatapp/src/app/interfaces/Message.ts
@@ -2,8 +2,10 @@ export class Message {
     message: string;
     isSenderServer: boolean;
     sent: boolean;
+    continuous?: boolean;
 
     constructor() {
         this.sent = false;
+        this.continuous = true;
     }
 }
